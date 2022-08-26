fb4a3791a89e02f375e3e9878a724ec5c39c568b
M. Naveed Qamar <naveedqamar6500@gmail.com>
2022-08-27 00:18:05 +0500

    Initial Comment for adding Files

---
+++ GridToExcelUsingClosedXML/GridToExcelUsingClosedXML/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using ClosedXML.Excel;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Input;
+using DocumentFormat.OpenXml.Drawing.Charts;
+using Microsoft.Win32;
+using System.Text;
+
+namespace GridToExcelUsingClosedXML
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<User> users = new List<User>();
+            users.Add(new User() { Id = 1, Name = "John Doe", Birthday = new DateTime(1971, 7, 23) });
+            users.Add(new User() { Id = 2, Name = "Jane Doe", Birthday = new DateTime(1974, 1, 17) });
+            users.Add(new User() { Id = 3, Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2) });
+
+            dgSimple.ItemsSource = users;
+        }
+
+        public class User
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public DateTime Birthday { get; set; }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var filename = "";
+
+            try
+            {
+                dgSimple.SelectionMode = DataGridSelectionMode.Extended;
+                dgSimple.SelectAllCells();
+
+                Clipboard.Clear();
+                ApplicationCommands.Copy.Execute(null, dgSimple);
+
+                var saveFileDialog = new SaveFileDialog
+                {
+                    FileName = filename != "" ? filename : "gpmfca-exportedDocument",
+                    DefaultExt = ".csv",
+                    Filter = "Common Seprated Documents (.csv)|*.csv"
+                };
+
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    var clip2 = Clipboard.GetText();
+                    File.WriteAllText(saveFileDialog.FileName, clip2.Replace('\t', ','), Encoding.UTF8);
+                    Process.Start(saveFileDialog.FileName);
+                }
+
+                dgSimple.UnselectAllCells();
+                dgSimple.SelectionMode = DataGridSelectionMode.Single;
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
---
+++ WPFDataGridControl/WPFDataGridControl/MainWindow.xaml.cs
@@ -0,0 +1,218 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Data;
+using System.Reflection;
+using Window = Microsoft.Office.Interop.Excel.Window;
+using System.Windows.Controls;
+using Range = Microsoft.Office.Interop.Excel.Range;
+
+namespace WPFDataGridControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<User> users = new List<User>();
+            users.Add(new User() { Id = 1, Name = "John Doe", Birthday = new DateTime(1971, 7, 23) });
+            users.Add(new User() { Id = 2, Name = "Jane Doe", Birthday = new DateTime(1974, 1, 17) });
+            users.Add(new User() { Id = 3, Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2) });
+
+            dgSimple.ItemsSource = users;
+        }
+        public class User
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public DateTime Birthday { get; set; }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            excel.Visible = true; //www.yazilimkodlama.com
+            Workbook workbook = excel.Workbooks.Add(System.Reflection.Missing.Value);
+            Worksheet sheet1 = (Worksheet)workbook.Sheets[1];
+
+            for (int j = 0; j < dgSimple.Columns.Count; j++) //Başlıklar için
+            {
+                Range myRange = (Range)sheet1.Cells[1, j + 1];
+                sheet1.Cells[1, j + 1].Font.Bold = true; //Başlığın Kalın olması için
+                sheet1.Columns[j + 1].ColumnWidth = 15; //Sütun genişliği ayarı
+                myRange.Value2 = dgSimple.Columns[j].Header;
+            }
+            for (int i = 0; i < dgSimple.Columns.Count; i++)
+            { //www.yazilimkodlama.com
+                for (int j = 0; j < dgSimple.Items.Count; j++)
+                {
+                    TextBlock b = dgSimple.Columns[i].GetCellContent(dgSimple.Items[j]) as TextBlock;
+                    Microsoft.Office.Interop.Excel.Range myRange = (Microsoft.Office.Interop.Excel.Range)sheet1.Cells[j + 2, i + 1];
+                    myRange.Value2 = b.Text;
+                }
+            }
+        }
+
+        dynamic Window.Activate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic ActivateNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic ActivatePrevious()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Close(object SaveChanges, object Filename, object RouteWorkbook)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic LargeScroll(object Down, object Up, object ToRight, object ToLeft)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Window NewWindow()
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic _PrintOut(object From, object To, object Copies, object Preview, object ActivePrinter, object PrintToFile, object Collate, object PrToFileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic PrintPreview(object EnableChanges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic ScrollWorkbookTabs(object Sheets, object Position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic SmallScroll(object Down, object Up, object ToRight, object ToLeft)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PointsToScreenPixelsX(int Points)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PointsToScreenPixelsY(int Points)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic RangeFromPoint(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ScrollIntoView(int Left, int Top, int Width, int Height, object Start)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic PrintOut(object From, object To, object Copies, object Preview, object ActivePrinter, object PrintToFile, object Collate, object PrToFileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Microsoft.Office.Interop.Excel.Application Application => throw new NotImplementedException();
+
+        public XlCreator Creator => throw new NotImplementedException();
+
+        dynamic Window.Parent => throw new NotImplementedException();
+
+        public Microsoft.Office.Interop.Excel.Range ActiveCell => throw new NotImplementedException();
+
+        public Chart ActiveChart => throw new NotImplementedException();
+
+        public Pane ActivePane => throw new NotImplementedException();
+
+        public dynamic ActiveSheet => throw new NotImplementedException();
+
+        public dynamic Caption { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayFormulas { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayGridlines { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayHeadings { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayHorizontalScrollBar { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayOutline { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool _DisplayRightToLeft { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayVerticalScrollBar { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayWorkbookTabs { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayZeros { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool EnableResize { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool FreezePanes { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int GridlineColor { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public XlColorIndex GridlineColorIndex { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int Index => throw new NotImplementedException();
+
+        public string OnWindow { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Panes Panes => throw new NotImplementedException();
+
+        public Microsoft.Office.Interop.Excel.Range RangeSelection => throw new NotImplementedException();
+
+        public int ScrollColumn { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int ScrollRow { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Sheets SelectedSheets => throw new NotImplementedException();
+
+        public dynamic Selection => throw new NotImplementedException();
+
+        public bool Split { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int SplitColumn { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double SplitHorizontal { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int SplitRow { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double SplitVertical { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double TabRatio { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public XlWindowType Type => throw new NotImplementedException();
+
+        public double UsableHeight => throw new NotImplementedException();
+
+        public double UsableWidth => throw new NotImplementedException();
+
+        public bool Visible { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Microsoft.Office.Interop.Excel.Range VisibleRange => throw new NotImplementedException();
+
+        public int WindowNumber => throw new NotImplementedException();
+
+        XlWindowState Window.WindowState { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public dynamic Zoom { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public XlWindowView View { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayRightToLeft { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public SheetViews SheetViews => throw new NotImplementedException();
+
+        public dynamic ActiveSheetView => throw new NotImplementedException();
+
+        public bool DisplayRuler { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool AutoFilterDateGrouping { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DisplayWhitespace { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int Hwnd => throw new NotImplementedException();
+    }
+}
